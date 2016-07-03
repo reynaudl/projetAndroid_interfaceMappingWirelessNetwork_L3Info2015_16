3f2ae22227dfcef6ea02b7027920a2be9457cf51
reynaudl <reynaudlc84@hotmail.fr>
2016-07-03 16:25:01 +0200

    Update mapping + wifi scan

--- MappingWifi/app/src/main/java/com/example/boyrie/mappingwifi/MapActivity.java
+++ MappingWifi/app/src/main/java/com/example/boyrie/mappingwifi/MapActivity.java
@@ -1,21 +1,74 @@
 package com.example.boyrie.mappingwifi;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Boyrie on 20/06/2016.
  */
 public class MapActivity extends Activity {
+
+    WifiManager wifiManagerHeatMap;
+    ListView listViewWifi;
+    TextView textStatus;
+    Button buttonScan;
+    int size = 0;
+    List<ScanResult> results;
+
+    String ITEM_KEY = "key";
+    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interface_map);
+
+
+//        textStatus = (TextView) findViewById(R.id.textStatus);
+//        buttonScan = (Button) findViewById(R.id.buttonScan);
+//        buttonScan.setOnClickListener(this);
+        listViewWifi = (ListView)findViewById(R.id.listView);
+
+        //Gestion de la détection des réseaux wifi
+        wifiManagerHeatMap = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifiManagerHeatMap.isWifiEnabled() == false)
+        {
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+            wifiManagerHeatMap.setWifiEnabled(true);
+        }
+         this.adapter = new SimpleAdapter(MapActivity.this, arraylist, R.layout.interface_map, new String[] { ITEM_KEY }, new int[] { R.id.listView });
+        listViewWifi.setAdapter(this.adapter);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context c, Intent intent)
+            {
+                results = wifiManagerHeatMap.getScanResults();
+                size = results.size();
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
 
         //Bloque la rotation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -45,15 +98,15 @@ public class MapActivity extends Activity {
                         startActivity(myIntent);
                         break;
 
-                case R.id.btnMap:
-                    myIntent = new Intent(MapActivity.this, HeatMapsActivity.class);
-                    startActivity(myIntent);
-                    break;
-
-                case R.id.btnWifi:
+                case R.id.btnParametre:
                     myIntent = new Intent(MapActivity.this, ParametreActivity.class);
                     startActivity(myIntent);
                     break;
+
+//                case R.id.btnWifi:
+//                    myIntent = new Intent(MapActivity.this, ParametreActivity.class);
+//                    startActivity(myIntent);
+//                    break;
                 }
             }
         };
